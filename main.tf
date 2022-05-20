4dde41d85ca37afa5ff4dcd974b25fafe564130e
peada04 <dpeacock@hashicorp.com>
2022-05-20 08:11:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "vpc" {
   }
 }
 
-resource "aws_db_subnet_group" "dap-vpc" {
+resource "aws_db_subnet_group" "dap-edu" {
   name       = "dap-db-subnet-group"
   subnet_ids = module.vpc.public_subnets
 
@@ -96,7 +96,7 @@ resource "aws_db_instance" "education" {
   engine_version         = "13.1"
   username               = "edu"
   password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.education.name
+  db_subnet_group_name   = aws_db_subnet_group.dap-edu.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   parameter_group_name   = aws_db_parameter_group.education.name
   publicly_accessible    = true
